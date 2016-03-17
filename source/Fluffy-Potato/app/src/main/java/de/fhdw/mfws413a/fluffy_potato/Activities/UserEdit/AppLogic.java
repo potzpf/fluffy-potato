fc2de59fca2a916f5b192e9c0f146232a43d5f96
dfwip <Dominik-felske@online.de>
2016-03-17 22:22:55 +0100

    neu layouts als klassen angelegt und ins manifest eingetragen

---
+++ source/Fluffy-Potato/app/src/main/java/de/fhdw/mfws413a/fluffy_potato/Activities/UserEdit/AppLogic.java
@@ -0,0 +1,25 @@
+package de.fhdw.mfws413a.fluffy_potato.Activities.UserEdit;
+
+import de.fhdw.mfws413a.fluffy_potato.Navigation.Navigation;
+
+public class AppLogic {
+
+    //vars
+
+    private Data mData;
+    private Gui mGui;
+
+    //Methods
+
+    public AppLogic(Data pData, Gui pGui) {
+
+        mData = pData;
+        mGui = pGui;
+
+    }
+
+    public void OnButtonStartClick(){
+        //System.out.println("Button Start clicked");
+        Navigation.startActivityUserSelection(mData.getActivity(),"");
+    }
+}
